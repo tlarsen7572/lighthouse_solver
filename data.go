0d43c3c2aee10a72e2146d969912a5c49287744c
Tom Larsen <larsenthomasj@gmail.com>
2021-10-25 20:23:22 -0400

    add matching functions

--- data.go
+++ data.go
@@ -9,6 +9,11 @@ const MarbleheadBottom = -100
 const FortNiagaraTop = 1000
 const FortNiagaraBottom = -1000
 
+const left = 0
+const top = 1
+const right = 2
+const bottom = 3
+
 type Card struct {
 	Id    int
 	Parts [4]int
@@ -22,14 +27,35 @@ func (c *Card) Rotate() {
 	c.Parts[0] = first
 }
 
+func (c *Card) MatchesRight(other *Card) bool {
+	return c.Parts[left]+other.Parts[right] == 0
+}
+
+func (c *Card) MatchesBottom(other *Card) bool {
+	return c.Parts[top]+other.Parts[bottom] == 0
+}
+
+func (c *Card) MatchesRightAndBottom(toLeft *Card, toTop *Card) bool {
+	return c.Parts[top]+c.Parts[left]+toLeft.Parts[right]+toTop.Parts[bottom] == 0
+}
+
+func (c *Card) HasPart(part int) bool {
+	for _, value := range c.Parts {
+		if value == part {
+			return true
+		}
+	}
+	return false
+}
+
 var cards = []*Card{
+	{Id: 1, Parts: [4]int{RoundIslandTop, FortNiagaraBottom, SplitRockBottom, MarbleheadBottom}},
+	{Id: 2, Parts: [4]int{FortNiagaraBottom, MarbleheadTop, SplitRockTop, RoundIslandTop}},
+	{Id: 3, Parts: [4]int{MarbleheadTop, RoundIslandBottom, FortNiagaraTop, SplitRockBottom}},
+	{Id: 4, Parts: [4]int{SplitRockBottom, MarbleheadTop, RoundIslandBottom, FortNiagaraTop}},
+	{Id: 5, Parts: [4]int{RoundIslandTop, SplitRockTop, MarbleheadBottom, SplitRockTop}},
 	{Id: 6, Parts: [4]int{MarbleheadTop, RoundIslandTop, MarbleheadBottom, FortNiagaraTop}},
 	{Id: 7, Parts: [4]int{MarbleheadTop, SplitRockBottom, RoundIslandBottom, FortNiagaraTop}},
-	{Id: 5, Parts: [4]int{RoundIslandTop, SplitRockTop, MarbleheadBottom, SplitRockTop}},
 	{Id: 8, Parts: [4]int{RoundIslandTop, SplitRockBottom, FortNiagaraTop, MarbleheadTop}},
-	{Id: 4, Parts: [4]int{SplitRockBottom, MarbleheadTop, RoundIslandBottom, FortNiagaraTop}},
-	{Id: 3, Parts: [4]int{MarbleheadTop, RoundIslandBottom, FortNiagaraTop, SplitRockBottom}},
-	{Id: 2, Parts: [4]int{FortNiagaraBottom, MarbleheadTop, SplitRockTop, RoundIslandTop}},
-	{Id: 1, Parts: [4]int{RoundIslandTop, FortNiagaraBottom, SplitRockBottom, MarbleheadBottom}},
 	{Id: 9, Parts: [4]int{MarbleheadBottom, RoundIslandTop, FortNiagaraTop, SplitRockTop}},
 }
--- data_test.go
+++ data_test.go
@@ -1,28 +1,66 @@
-package lighthouse_solver_test
+package lighthouse_solver
 
 import (
-	"lighthouse_solver"
 	"testing"
 )
 
 func TestRotateCard(t *testing.T) {
-	card := lighthouse_solver.Card{
+	card := Card{
 		Id:    1,
-		Parts: [4]int{lighthouse_solver.SplitRockTop, lighthouse_solver.RoundIslandTop, lighthouse_solver.MarbleheadTop, lighthouse_solver.FortNiagaraTop},
+		Parts: [4]int{SplitRockTop, RoundIslandTop, MarbleheadTop, FortNiagaraTop},
 	}
 
 	card.Rotate()
 
-	if part := card.Parts[0]; part != lighthouse_solver.FortNiagaraTop {
-		t.Fatalf(`expected %v but got %v`, lighthouse_solver.FortNiagaraTop, part)
+	if part := card.Parts[0]; part != FortNiagaraTop {
+		t.Fatalf(`expected %v but got %v`, FortNiagaraTop, part)
 	}
-	if part := card.Parts[1]; part != lighthouse_solver.SplitRockTop {
-		t.Fatalf(`expected %v but got %v`, lighthouse_solver.SplitRockTop, part)
+	if part := card.Parts[1]; part != SplitRockTop {
+		t.Fatalf(`expected %v but got %v`, SplitRockTop, part)
 	}
-	if part := card.Parts[2]; part != lighthouse_solver.RoundIslandTop {
-		t.Fatalf(`expected %v but got %v`, lighthouse_solver.RoundIslandTop, part)
+	if part := card.Parts[2]; part != RoundIslandTop {
+		t.Fatalf(`expected %v but got %v`, RoundIslandTop, part)
 	}
-	if part := card.Parts[3]; part != lighthouse_solver.MarbleheadTop {
-		t.Fatalf(`expected %v but got %v`, lighthouse_solver.MarbleheadTop, part)
+	if part := card.Parts[3]; part != MarbleheadTop {
+		t.Fatalf(`expected %v but got %v`, MarbleheadTop, part)
+	}
+}
+
+func TestMatchCard(t *testing.T) {
+	card := &Card{
+		Id:    1,
+		Parts: [4]int{SplitRockTop, RoundIslandTop, MarbleheadTop, FortNiagaraTop},
+	}
+	cardToLeft := &Card{
+		Id:    2,
+		Parts: [4]int{SplitRockTop, SplitRockTop, SplitRockBottom, SplitRockTop},
+	}
+	cardToTop := &Card{
+		Id:    3,
+		Parts: [4]int{SplitRockTop, SplitRockTop, SplitRockTop, RoundIslandBottom},
+	}
+
+	if !card.MatchesRight(cardToLeft) {
+		t.Fatalf(`expected card's left to match cardToLeft's right but it did not`)
+	}
+	if !card.MatchesBottom(cardToTop) {
+		t.Fatalf(`expected card's top to match cardToTop's bottom but it did not`)
+	}
+	if !card.MatchesRightAndBottom(cardToLeft, cardToTop) {
+		t.Fatalf(`expected card to match to cardToLeft and cardToTop but it did not`)
+	}
+}
+
+func TestHasPart(t *testing.T) {
+	card := &Card{
+		Id:    1,
+		Parts: [4]int{SplitRockTop, RoundIslandBottom, SplitRockTop, SplitRockTop},
+	}
+
+	if !card.HasPart(RoundIslandBottom) {
+		t.Fatalf(`card reported it does not have RoundIslandBottom, but it does`)
+	}
+	if card.HasPart(MarbleheadTop) {
+		t.Fatalf(`card reported it has MarbleheadTop, but it does not`)
 	}
 }
